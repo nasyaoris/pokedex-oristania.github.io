22c74fb8d7c1e154b1c4dbf0263efa1ddd92337f
Oristania Wahyu Nabasya <t-oristania.nabasya@ovo.id>
2021-03-25 23:35:08 +0700

    initiate boilerplate

---
+++ src/containers/App/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import React from "react";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "./theme";
+import { routes } from "./routes";
+
+function App() {
+    const pages = routes.map((route, i) => (
+      <Route
+        component={route.component}
+        exact={route.exact}
+        path={route.path}
+        key={i}
+      />
+    ));
+    return (
+      <ThemeProvider theme={theme}>
+        <Switch>{pages}</Switch>
+      </ThemeProvider>
+    );
+}
+
+export default withRouter(
+  connect(
+    null,
+    null
+  )(App)
+);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
+import { ConnectedRouter } from "connected-react-router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./index.css";
+// import "normalize.css";
+
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+
+const target = document.querySelector("#root");
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  target
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
